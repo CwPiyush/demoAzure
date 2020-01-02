64116c160583e396fee9698e5218cbe99e72b97e
Sahu, Piyush <piyush.sahu@cdk.com>
2020-01-02 18:34:41 +0530

    initial commit

---
+++ LearningNetcoreWebApp/LearningNetcoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearningNetcoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
